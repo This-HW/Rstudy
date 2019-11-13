f91a85ea6a09fb0bf590c082e4a8745ec7e9b19f
Yong-Ju <didix@naver.com>
2019-11-14 01:27:19 +0900

    2019. 11. 13 R study

---
+++ 수업 자료/Day17.R
@@ -0,0 +1,41 @@
+
+library("dplyr")
+
+
+df <-read.csv("./data/Abc1115.csv")
+head(df)
+
+names(df) <- c("std_no", "email", "kor", "eng", "math", "sci", "hist", "total", "mgr_code", "acc_code", "local_code")
+df <- df[,-12]
+head(df)
+
+
+#문제1
+df_b <- df %>% filter(local_code == "B")
+df_b <- df %>% mutate(temp = kor+eng)
+df_b <- df_b %>% arrange(desc(temp), std_no)
+head(df_b)
+df_b
+df[5,]%>% select(std_no)
+
+#문제2
+df_b %>% arrange(desc(temp)) %>% head(1) %>% select(temp)
+
+#문제3
+df_c <- df %>% mutate(temp = eng + math)
+score <- c(5, 15, 20)
+mgr_code <- c("A", "B", "C")
+temp1 <- data.frame(score,mgr_code)
+df_c <- left_join(x = df_c,y = temp1, by="mgr_code")
+df_c %>% filter(temp>=120) %>% select(total, score) %>% summarise(sum=sum(total)+sum(score))
+
+#문제4
+score <- c(5, 10, 15)
+local_code <- c("A", "B", "C")
+temp2 <- data.frame(score, local_code)
+df_l <- left_join(df, temp2, "local_code")
+df_l %>% filter(local_code=="A" | local_code== "B") %>% 
+  mutate(temp=kor+score) %>% 
+  filter(temp>=50) %>% count()
+
+
